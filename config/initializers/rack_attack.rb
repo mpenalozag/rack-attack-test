e09e4dccd27d0a22c3418566738012e81fb9e1b3
Martín Peñaloza <mpenaloza@uc.cl>
2025-08-06 12:27:05 -0400

    feat: config rack_attack

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -11,13 +11,13 @@ class Rack::Attack
   # ActiveSupport::Cache::Store
 
   # Configure Rack Attack to use a separate Redis instance
-  Rack::Attack.cache.store = ActiveSupport::Cache::RedisCacheStore.new(
-    url: ENV.fetch("REDIS_RATE_LIMIT_URL", "redis://localhost:6379/1"),
-    namespace: "rack-attack",
-    error_handler: ->(method:, returning:, exception:) { # rubocop:disable Lint/UnusedBlockArgument
-      Rails.logger.warn "Rack::Attack Redis error in #{method}: #{exception.message}"
-    }
-  )
+  # Rack::Attack.cache.store = ActiveSupport::Cache::RedisCacheStore.new(
+  #   url: ENV.fetch("REDIS_RATE_LIMIT_URL", "redis://localhost:6379/1"),
+  #   namespace: "rack-attack",
+  #   error_handler: ->(method:, returning:, exception:) { # rubocop:disable Lint/UnusedBlockArgument
+  #     Rails.logger.warn "Rack::Attack Redis error in #{method}: #{exception.message}"
+  #   }
+  # )
 
   ### Throttle Spammy Clients ###
 
@@ -44,7 +44,8 @@ class Rack::Attack
 
   # Track requests by authorization header (monitoring only)
   track("auth_header_requests", limit: 1, period: 1.second) do |req|
-    if req.path.start_with?("/v1/")
+    puts "req.path: #{req.path}"
+    if req.path.start_with?("/v1")
       auth_header = req.env["HTTP_AUTHORIZATION"]
       Digest::SHA512.hexdigest(auth_header) if auth_header.present?
     end
